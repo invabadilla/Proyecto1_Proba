203ab97142dd47229327a773fc458abcd2e96640
invabadilla <invabadilla@estudiantec.cr>
2023-02-20 12:49:32 -0600

    Initial box plot

--- RH_3.py
+++ RH_3.py
@@ -9,14 +9,24 @@ Template con lectura de datos en archivo csv
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-input_dir='C:/Users/PATH/' #PATH al archivo de datos, cambiar según cada computadora. Sirve para evitar 'File not found'
-filename=input_dir+'NOMBRE_de_ARCHIVO.csv'
+#input_dir='C:/Users/PATH/' #PATH al archivo de datos, cambiar según cada computadora. Sirve para evitar 'File not found'
+filename='energydata_complete.csv'
 
 # Esta línea lee la matriz de datos (sin titulos) para números solamente. Otro tipo de variable (texto por ejemplo) se leerá como nan
-datos=np.genfromtxt(filename,delimiter=',',skip_header=1)
+#datos=np.genfromtxt(filename,delimiter=';',skip_header=1, dtype=float)
 
 #alternativamente, se pueden leer columnas específicas entre el rango [X,Y] de esta forma:
-#datos=np.genfromtxt(filename,delimiter=',',skip_header=1, usecols = range(X,Y))
+datos=np.genfromtxt(filename,delimiter=';',skip_header=1, usecols = range(8,9), dtype=float)
+print(datos)
 
 # Su código va aquí...
+
+
+#Diagrama de cajas
+fig = plt.figure(figsize =(30, 10))
+plt.boxplot(datos, vert = False)
+plt.xlabel("humedad")
+plt.title("Diagrama de cajas humedad en el área de lavado")
+plt.show()
