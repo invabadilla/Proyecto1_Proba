b1585b6baf932ad42dbaff445b5a809973584b89
invabadilla <invabadilla@estudiantec.cr>
2023-02-20 13:07:58 -0600

    Histogram plot

--- RH_3.py
+++ RH_3.py
@@ -7,6 +7,7 @@ Tarea #1
 Template con lectura de datos en archivo csv
 
 """
+import math
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,4 +30,15 @@ fig = plt.figure(figsize =(30, 10))
 plt.boxplot(datos, vert = False)
 plt.xlabel("humedad")
 plt.title("Diagrama de cajas humedad en el área de lavado")
-plt.show()
+#plt.show()
+
+
+#Histograma
+intervalos = range(math.floor(min(datos)), math.ceil(max(datos)) + 2) #calculamos los extremos de los intervalos
+plt.hist(x=datos, bins=intervalos, color='#F2AB6D', rwidth=0.85)
+plt.title('Histograma de humedad en el área de lavado')
+plt.xlabel('Huemadad')
+plt.ylabel('Frecuencia')
+plt.xticks(intervalos)
+
+plt.show() #dibujamos el histograma
